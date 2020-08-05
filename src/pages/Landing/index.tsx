ade01ff71b9dd9101824d8fb301a3f95f18d8e0e
gabriel.bucalon <bucalongabriell@gmail.com>
2020-08-04 22:42:03 -0300

    feat and fix: create component pageGeader and adjust css styles inputs/header

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,49 +1,47 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom'
 
-import "./styles.css";
+import logoImg from '../../assets/images/logo.svg';
+import ladingImg from '../../assets/images/landing.svg';
+import studyIcon from '../../assets/images/icons/study.svg';
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
-import logoImg from "../../assets/images/logo.svg";
-import landingImg from "../../assets/images/landing.svg";
-import studyIcon from "../../assets/images/icons/study.svg";
-import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
-import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
-import { Link } from "react-router-dom";
+import './styles.css';
 
-export default function Landing() {
-  return (
-    <div id="page-landing">
-      <div className="container" id="page-landing-content">
-        <div className="logo-container">
-          <img src={logoImg} alt="Proffy" />
-          <h2>Sua Plataforma de estudos online</h2>
+function Landing() {
+    return (
+        <div id="page-landing">
+            <div id="page-landing-content" className="container">
+                <div className="logo-container">
+                    <img src={logoImg} alt="Proffy"/>
+                    <h2>Your online study platform.</h2>
+                </div>
+
+                <img 
+                    src={ladingImg} 
+                    alt="Plataforma de estudos online." 
+                    className="hero-image"
+                />
+
+                <div className="buttons-container">
+                    <Link to="/study" className="study">
+                        <img src={studyIcon} alt="Study"/>
+                        Study
+                    </Link>
+
+                    <Link to="/give-classes" className="give-classes">
+                        <img src={giveClassesIcon} alt="Give Lessons"/>
+                        Teach
+                    </Link>
+                </div>
+                <span className="total-connections">
+                    Total of 834 connections made  
+                    <img src={purpleHeartIcon} alt="Purple Heart"/>
+                </span>
+            </div>
         </div>
-
-        <img
-          src={landingImg}
-          alt="Plataforma de Estudos"
-          className="hero-image"
-        />
-
-        <div className="buttons-container">
-          
-        <Link className="study" to="study">
-          <img src={studyIcon} alt="Estudar" />
-            Estudar
-        </Link>
-            
-        <Link className="give-classes" to="give-classes">
-            <img src={giveClassesIcon} alt="Dar Aulas" />
-            Dar Aulas
-        </Link>
-            
-          
-        </div>
-
-        <span className="total-connections">
-          Total de 200 conexões realizadas.{" "}
-          <img src={purpleHeartIcon} alt="Coração Roxo" />
-        </span>
-      </div>
-    </div>
-  );
+    )
 }
+
+export default Landing;
